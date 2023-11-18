f03eb8847c00618df3d5121106cd260c6af32749
Nick8449 <ovchynnik.mykola@gmail.com>
2023-11-18 17:02:41 +0100

    add/iziToast&slimselect_libs

--- src/index.js
+++ src/index.js
@@ -1,19 +1,48 @@
+import SlimSelect from 'slim-select';
+import iziToast from 'izitoast';
 import { fetchBreed, fetchCatByBreed } from './cat-api';
+
+import '../node_modules/slim-select/dist/slimselect.css';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const elements = {
   select: document.querySelector('.breed-select'),
-  msgLoader: document.querySelector('.loader'),
+  msgLoader: document.querySelector('.loader-text'),
+  loader: document.querySelector('.loader'),
   msgError: document.querySelector('.error'),
   catInfo: document.querySelector('.cat-info'),
 };
 
-elements.select.addEventListener('change', handlerSelect);
+// elements.select.addEventListener('change', handlerSelect);
 
 fetchBreed()
   .then(data => {
     elements.select.insertAdjacentHTML('afterbegin', selectMarkup(data));
+    elements.select.classList.remove('hidden');
+    elements.msgLoader.classList.add('hidden');
+    elements.loader.classList.add('hidden');
+
+    new SlimSelect({
+      select: elements.select,
+      events: {
+        afterChange: handlerSelect,
+      },
+    });
   })
-  .catch(err => console.log(err));
+  .catch(err => {
+    elements.msgLoader.classList.add('hidden');
+    elements.loader.classList.add('hidden');
+    new iziToast.error({
+      message: 'Oops! Something went wrong! Try reloading the page!',
+      position: 'topLeft',
+      icon: '',
+      messageColor: 'red',
+      backgroundColor: 'transparent',
+      displayMode: 0,
+      timeout: '',
+    });
+    console.log(err);
+  });
 
 function selectMarkup(array) {
   return array
@@ -22,6 +51,9 @@ function selectMarkup(array) {
 }
 
 function handlerSelect() {
+  elements.msgLoader.classList.remove('hidden');
+  elements.loader.classList.remove('hidden');
+  elements.catInfo.classList.add('hidden');
   fetchCatByBreed(elements.select.value)
     .then(
       ([
@@ -37,8 +69,24 @@ function handlerSelect() {
           <p class="cat-description">${description}</p>
           <p class="cat-temper"><b>Temperament:</b> ${temperament}</p>
         </div>`;
-        console.log(temperament, description, url);
+        elements.msgLoader.classList.add('hidden');
+        elements.loader.classList.add('hidden');
+        elements.catInfo.classList.remove('hidden');
       }
     )
-    .catch(err => console.log(err));
+    .catch(err => {
+      elements.msgLoader.classList.add('hidden');
+      elements.loader.classList.add('hidden');
+
+      iziToast.error({
+        message: 'Oops! Something went wrong! Try reloading the page!',
+        position: 'topLeft',
+        icon: '',
+        messageColor: 'red',
+        backgroundColor: 'white',
+        displayMode: 0,
+        timeout: '',
+      });
+      console.log(err);
+    });
 }
